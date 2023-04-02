fc13111ea7184d33a9182d61476620d434790241
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-04-03 00:32:50 +0700

    Schema Added

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,2 +1,19 @@
+import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experience from "./experience"
+import skills from "./skills"
 import testimonials from "./testimonials"
-export const schemaTypes = [testimonials]
+import work from "./work"
+import workExperience from "./workExperience"
+
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experience,
+    skills,
+    work,
+    workExperience,
+    testimonials
+]
