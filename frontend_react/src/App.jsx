20a0bd0a7562f0f1642991688995e17d8494f4e8
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-04-03 13:22:24 +0700

    Boiler Plate Structrue

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-// import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-// import { Navbar } from './components';
+import { About, Footer, Header, Skills, Testimonials, Work } from "./containers";
+import { Navbar } from './components';
+import './App.scss'
 
 const App = () => (
-  <div>
-    APP
-  </div>
-  // <div className="app">
-  //   <Navbar />
-  //   <Header />
-  //   <About />
-  //   <Work />
-  //   <Skills />
-  //   <Testimonial />
-  //   <Footer />
-  // </div>
+  <>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  </>
 );
 
 export default App;
---
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.scss'
+const Navbar = () => {
+  return (
+    <div>
+      NavBAr
+    </div>
+  )
+}
+
+export default Navbar
