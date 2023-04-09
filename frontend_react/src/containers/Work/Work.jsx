f96ca6698ee079f2410837d2408ef176b84b674c
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-04-09 18:26:25 +0700

    skills and portfolio fetched

--- frontend_react/src/containers/Work/Work.jsx
+++ frontend_react/src/containers/Work/Work.jsx
@@ -1,11 +1,183 @@
-import React from 'react'
-import './Work.scss'
-const Work = () => {
-  return (
-    <div>
-      Work
-    </div>
-  )
-}
+import React, { useState, useEffect } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { HiOutlineRocketLaunch } from "react-icons/hi2";
 
-export default Work
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Work.scss";
+
+const Work = () => {
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [showMore, setShowMore] = useState(false);
+  const [expandedId, setExpandedId] = useState(null);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+  // console.log(works[0]._id);
+  const toggleShowMore = (id) => {
+    if (id === expandedId) {
+      setExpandedId(null);
+    } else {
+      setExpandedId(id);
+    }
+    setShowMore(!showMore);
+  };
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === "All") {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
+
+      <div className="app__work-filter">
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 0.8] }}
+                    whileHover={{ scale: [0.8, 1] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                    title="View Website"
+                  >
+                    <HiOutlineRocketLaunch />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 0.8] }}
+                    whileHover={{ scale: [0.8, 1] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                    title="View Source Code"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+
+            {/* <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p
+                className={`p-text app__work-description ${
+                  showMore ? "app__work-description--expanded" : ""
+                }`}
+                style={{ marginTop: 10 }}
+              >
+                {work.description}
+              </p>
+              {work.description.length > 100 && (
+                <button onClick={() => setShowMore(!showMore)}>
+                  {showMore ? "See less" : "See more"}
+                </button>
+              )}
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div> */}
+            {/* <div className="app__work-card">
+              <div className="app__work-img-container">
+                <img
+                  src={work.image}
+                  alt={work.title}
+                  className="app__work-img"
+                />
+              </div>
+              <div className="app__work-content app__flex">
+                <h4 className="bold-text">{work.title}</h4>
+                <p
+                  className={`p-text app__work-description ${
+                    expandedId === work.id
+                      ? "app__work-description--expanded"
+                      : ""
+                  }`}
+                  style={{ marginTop: 10 }}
+                >
+                  {showMore && expandedId === work._id
+                    ? work.description
+                    : work.description.substring(0, 100) + "..."}
+                </p>
+                {work.description.length > 100 && (
+                  <button onClick={() => toggleShowMore(work.id)}>
+                    {expandedId === work.id ? "See less" : "See more"}
+                  </button>
+                )}
+                <div className="app__work-tag app__flex">
+                  <p className="p-text">{work.tags[0]}</p>
+                </div>
+              </div>
+            </div> */}
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
+
+export default AppWrap(Work, "work");
